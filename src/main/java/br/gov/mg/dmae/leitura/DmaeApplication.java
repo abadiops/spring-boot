1cff7129525eca77fa8b4f1a6379e1afc92f43d0
Abadio de paulo <abadiops@hotmail.com>
2021-11-25 21:25:12 -0300

    Project created

---
+++ src/main/java/br/gov/mg/dmae/leitura/DmaeApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mg.dmae.leitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mg/dmae/leitura/DmaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mg.dmae.leitura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
